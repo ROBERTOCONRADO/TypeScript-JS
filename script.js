d7a409538e2aed71e2aed32ae33a1829ad6f233b
Roberto Conrado <robertoconradorc@gmail.com>
2023-01-02 22:45:15 -0300

    inicial-TS

---
+++ script.js
@@ -0,0 +1,13 @@
+"use strict";
+function somar(a, b) {
+    return a + b;
+}
+console.log(somar(8, 10));
+function dividir(c, d) {
+    return c / d;
+}
+console.log(dividir(8, 10));
+function multiplicar(e, f) {
+    return e * f;
+}
+console.log(multiplicar(8, 10));
---
+++ script.ts
@@ -0,0 +1,14 @@
+function somar(a: number, b: number) {
+  return a + b;
+}
+console.log(somar(8, 10));
+
+function dividir(c: number, d: number) {
+  return c / d;
+}
+console.log(dividir(8, 10));
+
+function multiplicar(e: number, f: number) {
+  return e * f;
+}
+console.log(multiplicar(8, 10));
