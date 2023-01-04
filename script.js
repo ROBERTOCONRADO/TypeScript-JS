692e17bbb84060240543d1558c516fe8e648ea3b
Roberto Conrado <robertoconradorc@gmail.com>
2023-01-04 10:11:44 -0300

    resolução-exercício-2-TypeScript

--- script.js
+++ script.js
@@ -17,11 +17,41 @@ const preco = 100;
   Conserte a função com typescript
 
 function normalizarTexto(texto) {
-  return texto.trims().toLowerCase();
+  return texto.trims().toLowercase();
 }
 
 */
 function normalizarTexto(texto) {
     return texto.trim().toLowerCase();
 }
-console.log(normalizarTexto('ÓLA, SEJA BEM-VINDO(a).'));
+console.log(normalizarTexto(' ÓLA, SEJA BEM-VINDO(a). '));
+/*    Exercicio 2
+  Conserte as funções com typescript
+
+<h1>Bônus de 100 - 20% do total:</h1>
+<label>Total</label>
+<input type="number" />
+<p></p>
+
+*/
+const input = document.querySelector("input");
+const total = localStorage.getItem("total");
+if (input && total) {
+    input.value = total;
+    calcularGanho(+input.value);
+}
+function calcularGanho(value) {
+    const p = document.querySelector("p");
+    if (p) {
+        p.innerText = `ganho total: ${value + 100 - value * 0.2}`;
+    }
+}
+function totalMudou() {
+    if (input) {
+        localStorage.setItem("total", input.value);
+        calcularGanho(Number(input.value));
+    }
+}
+if (input) {
+    input.addEventListener("keyup", totalMudou);
+}
