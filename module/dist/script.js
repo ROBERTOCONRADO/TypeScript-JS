ac0ac666415739698b841a9a4b5fd56b691a4ef9
Roberto Conrado <robertoconradorc@gmail.com>
2023-01-18 11:30:46 -0300

    module-ts

---
+++ module/dist/script.js
@@ -0,0 +1,9 @@
+import pluginSlide from "./pluginSlide.js";
+import { URL_BASE } from "./global.js";
+pluginSlide("div");
+console.log(URL_BASE);
+const livro = {
+    nome: "str",
+    preco: 200,
+};
+//# sourceMappingURL=script.js.map
