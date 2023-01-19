42896b28dfccdfdb902058c758a3bb852e87378f
Roberto Conrado <robertoconradorc@gmail.com>
2023-01-19 13:54:15 -0300

    moeda-para-number

---
+++ pratica-dados/3-moeda-para-number/dist/normalizarTransacao.js
@@ -0,0 +1,15 @@
+import moedaParaNumero from "./moedaParaNumero.js";
+export default function normalizarTransacao(transacao) {
+    return {
+        nome: transacao.Nome,
+        id: transacao.ID,
+        data: transacao.Data,
+        status: transacao.Status,
+        email: transacao.Email,
+        moeda: transacao["Valor (R$)"],
+        valor: moedaParaNumero(transacao["Valor (R$)"]),
+        pagamento: transacao["Forma de Pagamento"],
+        novo: Boolean(transacao["Cliente Novo"]),
+    };
+}
+//# sourceMappingURL=normalizarTransacao.js.map
---
+++ pratica-dados/3-moeda-para-number/dist/script.js
@@ -0,0 +1,11 @@
+import fetchData from "./fetchData.js";
+import normalizarTransacao from "./normalizarTransacao.js";
+async function handleData() {
+    const data = await fetchData("https://api.origamid.dev/json/transacoes.json?");
+    if (!data)
+        return;
+    const transacoes = data.map(normalizarTransacao);
+    console.log(transacoes);
+}
+handleData();
+//# sourceMappingURL=script.js.map
