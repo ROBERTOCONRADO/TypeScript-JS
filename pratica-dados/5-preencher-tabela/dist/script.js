7b1daf03e01ef70b56cd476b1f51851ea1a9e511
Roberto Conrado <robertoconradorc@gmail.com>
2023-01-20 12:13:24 -0300

    preencher tabela

---
+++ pratica-dados/5-preencher-tabela/dist/script.js
@@ -0,0 +1,27 @@
+import fetchData from "./fetchData.js";
+import normalizarTransacao from "./normalizarTransacao.js";
+async function handleData() {
+    const data = await fetchData("https://api.origamid.dev/json/transacoes.json?");
+    if (!data)
+        return;
+    const transacoes = data.map(normalizarTransacao);
+    preencherTabela(transacoes);
+}
+function preencherTabela(transacoes) {
+    const tabela = document.querySelector("#transacoes tbody");
+    if (!tabela)
+        return;
+    transacoes.forEach((transacao) => {
+        tabela.innerHTML += `
+      <tr>
+        <td>${transacao.nome}</td>
+        <td>${transacao.email}</td>
+        <td>R$ ${transacao.moeda}</td>
+        <td>${transacao.pagamento}</td>
+        <td>${transacao.status}</td>
+      </tr>
+    `;
+    });
+}
+handleData();
+//# sourceMappingURL=script.js.map
