7551cfb77ecae048b8aaff6fd170a9cbe0e89d92
Roberto Conrado <robertoconradorc@gmail.com>
2023-01-21 10:25:32 -0300

    preeencher-lista

---
+++ pratica-dados/8-preencher-lista/dist/script.js
@@ -0,0 +1,49 @@
+import Estatiscas from "./Estatiscas.js";
+import fetchData from "./fetchData.js";
+import normalizarTransacao from "./normalizarTransacao.js";
+async function handleData() {
+    const data = await fetchData("https://api.origamid.dev/json/transacoes.json?");
+    if (!data)
+        return;
+    const transacoes = data.map(normalizarTransacao);
+    preencherTabela(transacoes);
+    preencherEstatisticas(transacoes);
+}
+function preencherLista(lista, containerId) {
+    const containerElement = document.getElementById(containerId);
+    if (containerElement) {
+        Object.keys(lista).forEach((key) => {
+            containerElement.innerHTML += `<p>${key}: ${lista[key]}</p>`;
+        });
+    }
+}
+function preencherEstatisticas(transacoes) {
+    const data = new Estatiscas(transacoes);
+    preencherLista(data.pagamento, "pagamento");
+    preencherLista(data.status, "status");
+    const totalElement = document.querySelector("#total span");
+    if (totalElement) {
+        totalElement.innerText = data.total.toLocaleString("pt-BR", {
+            style: "currency",
+            currency: "BRL",
+        });
+    }
+}
+function preencherTabela(transacoes) {
+    const tabela = document.querySelector("#transacoes tbody");
+    if (!tabela)
+        return;
+    transacoes.forEach((transacao) => {
+        tabela.innerHTML += `
+      <tr>
+        <td>${transacao.nome}</td>
+        <td>${transacao.email}</td>
+        <td>R$ ${transacao.moeda}</td>
+        <td>${transacao.pagamento}</td>
+        <td>${transacao.status}</td>
+      </tr>
+    `;
+    });
+}
+handleData();
+//# sourceMappingURL=script.js.map
